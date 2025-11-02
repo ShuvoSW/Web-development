12154aa24be39f0a5a651a118a6677ad87a9d79c
ShuvoSW <shuvomajumder3369@gmail.com>
2025-11-02 21:44:51 +0600

    interval,event,binning data

--- M - 2/tempCodeRunnerFile.js
+++ M - 2/tempCodeRunnerFile.js
@@ -1,10 +1,2 @@
-const course1 = { name: "Programming Hero" };
-// const course2 = { name: "Next Level Web Development" };
 
-// const obj = {
-//     // nextLevel: {courseId: "level2"},
-//     // "Programming Hero": {courseId: "level1"}
-
-// };
-// obj[course2] = { courseId: "level2" };
-// obj[course1] = { courseId: "level1" };
+//   "2025-10-22T11:00:00.000Z": { "total": 1 }
