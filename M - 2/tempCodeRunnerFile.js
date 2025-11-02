64df829797dcc6aba1ef8637b8adb61d336a8940
ShuvoSW <shuvomajumder3369@gmail.com>
2025-11-02 18:59:56 +0600

    lookupTable

---
+++ M - 2/tempCodeRunnerFile.js
@@ -0,0 +1,10 @@
+const course1 = { name: "Programming Hero" };
+// const course2 = { name: "Next Level Web Development" };
+
+// const obj = {
+//     // nextLevel: {courseId: "level2"},
+//     // "Programming Hero": {courseId: "level1"}
+
+// };
+// obj[course2] = { courseId: "level2" };
+// obj[course1] = { courseId: "level1" };
