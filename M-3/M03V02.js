3f66b13af4b2f9c0f81f85f2f5516368a1ce582d
ShuvoSW <shuvomajumder3369@gmail.com>
2025-11-03 21:58:03 +0600

    Class

--- M-3/M03V02.js
+++ M-3/M03V02.js
@@ -13,16 +13,47 @@
 
 // Closer
 
-const createCounter = () => {
-    let count = 0;
+// const createCounter = () => {
+//     let count = 0;
 
-    return (amount) => {
-         count = count + amount;
-         return count
+//     return (amount) => {
+//         count = count + amount;
+//         return count
+//     }
+// };
+
+// const counter = createCounter();
+
+// console.log(counter(3))
+// console.log(counter(5))
+
+// Class
+
+class Counter {
+    constructor(count) {
+        this.count = count;
     }
-};
 
-const  counter = createCounter();
+    add(amount) {
+        this.count = this.count + amount
+    }
 
-console.log(counter(3))
-console.log(counter(5))
+    print() {
+        console.log(this.count)
+    }
+
+}
+
+const counter1 =  new Counter(0);
+
+counter1.add(2);
+counter1.add(3);
+
+counter1.print();
+
+const counter2 = new Counter(10);
+
+counter2.add(20)
+counter2.add(30)
+
+counter2.print()
