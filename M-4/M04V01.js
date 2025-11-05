d7da6d4d0e2766af48759c32713b9fa1eca25d7f
ShuvoSW <shuvomajumder3369@gmail.com>
2025-11-05 20:59:26 +0600

    id insert in map

--- M-4/M04V01.js
+++ M-4/M04V01.js
@@ -4,6 +4,8 @@
 // The goal is to store the results of a function call so that if the same call is made again,
 // the result is returned from the cache instead of running the expensive function.
 
+const dataCache = new Map();
+
 const expensiveTask = (id) => {
     console.log("Run the expensive task foe", id);
 
@@ -15,7 +17,19 @@ const expensiveTask = (id) => {
 };
 
 const getData = (id) => {
-    return expensiveTask(id);
+    if (dataCache.has(id)) {
+        console.log("Cache HIT for id:", id)
+        return dataCache.get(id);
+    }
+    
+    console.log("Cache MISS for id:", id)
+    const data = expensiveTask(id);
+
+    dataCache.set(id, data);
+
+    return data;
 }
 
 console.log(getData(123))
+
+console.log(dataCache)
