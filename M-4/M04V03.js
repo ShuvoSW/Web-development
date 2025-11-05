02a41cca77d4f6426185e7b798d1105da31bf9ff
ShuvoSW <shuvomajumder3369@gmail.com>
2025-11-05 23:01:58 +0600

    Stack

--- M-4/M04V03.js
+++ M-4/M04V03.js
@@ -1,3 +1,5 @@
+// import Stack from "./Stack";
+
 // Problem Statement
 
 // Given a string s containing just the characters "(", ")", "{", "}", "[" and "]",
@@ -12,3 +14,47 @@
 //? "([{}])" -> true
 //? "(]" -> false
 //? "(()" -> false
+
+class Stack {
+    constructor() {
+        this.items = [];
+    }
+
+    //* O(1)
+    push(value) {
+        this.items.push(value);
+    }
+
+    //* O(1)
+    pop() {
+        if (this.isEmpty()) {
+            return undefined;
+        }
+
+        return this.items.pop();
+    }
+
+    //* O(1)
+    peek() {
+        if (this.isEmpty()) {
+            return undefined;
+        }
+
+        return this.items[this.items.length - 1];
+    }
+
+    //* O(1)
+    isEmpty() {
+        return this.items.length === 0;
+    }
+
+    //* O(n)
+    print() {
+        console.log(this.items.slice().reverse().join(" -> "));
+    }
+}
+
+
+const stack = new Stack();
+
+console.log(stack);
