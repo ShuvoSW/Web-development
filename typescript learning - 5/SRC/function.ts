d5e59c7b8f74a07c3b52ab6b434aea225fcedd0f
ShuvoSW <shuvomajumder3369@gmail.com>
2025-11-09 18:28:51 +0600

    Object function method

--- typescript learning - 5/SRC/function.ts
+++ typescript learning - 5/SRC/function.ts
@@ -8,4 +8,17 @@ function addNormal(num1: number, num2: number): number {
 const addArrow = (num1: number, num2: number): 
 number => num1 + num2;
 
-addArrow(2, 2)
+addArrow(2, 2);
+
+// object => function => method
+
+const poorUser = {
+    name: "Shuvo",
+    balance: 0,
+    addBalance(value: number): number {
+        const totalBalance = this.balance + value;
+        return totalBalance;
+    },
+};
+
+poorUser.addBalance(100000)
