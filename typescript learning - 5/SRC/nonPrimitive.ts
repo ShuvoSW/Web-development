4d92b358fff22ec7817a470438b15742dcbe7d87
ShuvoSW <shuvomajumder3369@gmail.com>
2025-11-09 16:38:01 +0600

    Tuple

--- typescript learning - 5/SRC/nonPrimitive.ts
+++ typescript learning - 5/SRC/nonPrimitive.ts
@@ -1,4 +1,6 @@
 // array, object
+
+//- tuple
 let bazarList: string[] = ["eggs", "milk", "sugar"];
 
 bazarList.push(true)
@@ -6,3 +8,11 @@ bazarList.push(true)
 let mixedArr: (string | number)[] = ["eggs", 12, "milk", 1,  "sugar", 2];
 
 mixedArr.push(true)
+
+let coordinates: [number, number] = [20, 30];
+
+let couple: [string, string] = ["Husband", "Wife"];
+
+let ShuvoNameAndRoll: [string, number] = ["Shuvo", 79];
+
+let destination: [string, string, number] = ["Dhaka", "Chattogram", 5];
