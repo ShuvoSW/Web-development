52d124856acef058d18770d5b4e611245d5140fc
ShuvoSW <shuvomajumder3369@gmail.com>
2025-11-10 21:54:22 +0600

    Interface

--- M-6/SRC/inteface.ts
+++ M-6/SRC/inteface.ts
@@ -7,15 +7,54 @@ type Role = {
     role: "Admin" | "user";
 }
 
-type UserWithRole = User & Role;
+// interface : object type: array, object, function
 
-const user1: UserWithRole = {
+interface IUser {
+    name: string;
+    age: number;
+}
+
+interface IUserWithRole extends IUser {
+    role: "Admin" | "User"
+}
+
+const user1: IUserWithRole = {
     name: "Shuvo",
     age: 23,
     role: "Admin"
 }
 
-const user2: User = {
+type UserWithRole = User & Role;
+
+const user2: UserWithRole = {
+    name: "Shuvo",
+    age: 23,
+    role: "Admin"
+}
+
+const user3: IUser = {
     name: "Shuvo",
     age: 23
 }
+
+type IsAdmin = boolean
+
+const isAdmin : IsAdmin = true;
+
+// Function
+
+type Add = (num1: number, num2: number) => number;
+
+interface IAdd {
+    (num1: number, num2: number): number;
+}
+
+const add: IAdd = (num1, num2) => num1 + num2;
+
+type Friends = string[]; 
+
+interface IFriends {
+    [index: number]: string;
+}
+
+const friends: IFriends = ["1", "2", "3"];
