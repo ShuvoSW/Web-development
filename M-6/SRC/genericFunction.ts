cd2c2828f1c0cb7abef95eab939660e1d3fc2df7
ShuvoSW <shuvomajumder3369@gmail.com>
2025-11-11 11:45:37 +0600

    CreateArrayTupleWithGeneric

--- M-6/SRC/genericFunction.ts
+++ M-6/SRC/genericFunction.ts
@@ -17,3 +17,18 @@ const arrObj =  createArrayWithGeneric({
     id: 123, 
     name: "Shuvo"
 });
+
+// tuple
+
+const createArrayWithTuple = (param1: string, param2: string) => [param1, param2];
+
+const createArrayTupleWithGeneric = <X, Y>(param1: X,param2: Y) => [
+    param1,
+    param2
+];
+
+const res1 = createArrayTupleWithGeneric("Shuvo", false);
+
+const res2 = createArrayTupleWithGeneric(123, {
+    name: "Shuvo"
+});
