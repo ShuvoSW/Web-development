4072a4746fcc983f54488f938867bbbf267ee41c
ShuvoSW <shuvomajumder3369@gmail.com>
2025-11-11 21:04:19 +0600

    Mapped type

---
+++ M-6/SRC/mapped.ts
@@ -0,0 +1,50 @@
+// Mapped types
+
+// Map
+
+const arrayOfNumber: number[] = [1, 4, 6];
+
+const arrayOfString: string[] = ["1", "4", "6"];
+
+const arrayOfStringUsingMap: string[] = arrayOfNumber.map((num) => num.toString());
+
+console.log(arrayOfStringUsingMap)
+
+
+const user = {
+    id: 222
+}
+
+type ArrayOfNum = {
+    height: number;
+    width: number;
+};
+
+type height = ArrayOfNum["height"];
+
+// type arrayOfString = {
+//     height: string;
+//     width: string;
+// };
+
+// type arrayOfString = {
+//     [key in keyof ArrayOfNum]: string;
+// };
+// type arrayOfString = {
+//     [key in keyof ArrayOfNum]: string;
+// };
+// type arrayOfString = {
+//     [key in "height" | "width"]: string;
+// };
+// type arrayOfBool = {
+//     [key in "height" | "width"]: boolean;
+// };
+type Area<T> = {
+    [key in keyof T]: T[key];
+};
+
+const area1: Area<{height: string; width: string}> = {
+    height: 50,
+    width: 40,
+}
+
