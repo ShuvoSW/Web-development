8380c949c966226e8ec150ff7f21541fd7b5c95b
ShuvoSW <shuvomajumder3369@gmail.com>
2025-11-13 22:33:15 +0600

    Value use in method

---
+++ M-7/SRC/inheritence.ts
@@ -0,0 +1,20 @@
+class Student {
+    name: string;
+    age: number;
+    address: string;
+
+    constructor(name: string, age: number, address: string) {
+        this.name = name;
+        this.age = age;
+        this.address = address;
+    }
+
+    getSleep(numOfHours: number) {
+        console.log(`${this.name} ${numOfHours} ghota ghumai`);
+    }
+
+}
+
+const student1 = new Student(`Mr. Shuvo`, 18, "Bangladesh");
+
+student1.getSleep(15);
