e95ea2bc21d259a09535f74808d62bf65a5442eb
ShuvoSW <shuvomajumder3369@gmail.com>
2025-11-13 21:44:30 +0600

    First class

---
+++ M-7/SRC/class.ts
@@ -0,0 +1,17 @@
+// oop - class - object
+class Animal {
+    name: string;
+    species: string;
+    sound: string;
+
+
+    constructor(name: string, species: string, sound: string) {
+        this.name = name;
+        this.species = species;
+        this.sound = sound;
+    } 
+}
+
+const dog = new Animal("dogesh bhai", "dog", "Ghew ghew");
+
+console.log(dog.name)
