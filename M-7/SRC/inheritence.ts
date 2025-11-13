dfc039050c081298d732e052b27d4404beac3376
ShuvoSW <shuvomajumder3369@gmail.com>
2025-11-13 23:46:09 +0600

    Doubla class

--- M-7/SRC/inheritence.ts
+++ M-7/SRC/inheritence.ts
@@ -1,20 +1,47 @@
-class Student {
+// class Student {
+//     name: string;
+//     age: number;
+//     address: string;
+
+//     constructor(name: string, age: number, address: string) {
+//         this.name = name;
+//         this.age = age;
+//         this.address = address;
+//     }
+
+//     getSleep(numOfHours: number) {
+//         console.log(`${this.name} ${numOfHours} ghota ghumai`);
+//     }
+
+// }
+
+// const student1 = new Student(`Mr. Shuvo`, 18, "Bangladesh");
+
+// student1.getSleep(15);
+
+class Teacher {
     name: string;
     age: number;
     address: string;
+    designation: string; // extra property
 
-    constructor(name: string, age: number, address: string) {
+    constructor(name: string, age: number, address: string,  designation: string) {
         this.name = name;
         this.age = age;
         this.address = address;
+        this.designation = designation;
     }
 
     getSleep(numOfHours: number) {
         console.log(`${this.name} ${numOfHours} ghota ghumai`);
     }
 
+    takeClass(numOfClass: number) {
+        console.log(`${this.name} ${numOfClass} ghonta class nea`)
+    }
+
 }
 
-const student1 = new Student(`Mr. Shuvo`, 18, "Bangladesh");
+const teacher1 = new Teacher("Mr. smart teacher", 25, "Bangladesh", "teacher");
 
-student1.getSleep(15);
+teacher1.takeClass(2)
