1d3694f2f360c09c26ca908ce5b7e393eca5d88b
ShuvoSW <shuvomajumder3369@gmail.com>
2025-11-14 17:25:13 +0600

    Access modifiers

---
+++ M-7/SRC/accessmodifiers.ts
@@ -0,0 +1,85 @@
+// access >> modify
+
+class BankAccount {
+    // readonly userId: number;
+    public userId: number;
+    public userName: string;
+    // private userBalance: number;
+    protected userBalance: number;
+
+    constructor(userId: number, userName: string, userBalance: number) {
+        this.userId = userId;
+        this.userName = userName;
+        this.userBalance = userBalance;
+    }
+
+    protected addBalance(balance: number) {
+        this.userBalance = this.userBalance + balance;
+    }
+}
+
+class studentBankAccount extends BankAccount {
+    test() {
+        this.userBalance
+    }
+
+const mezbaBhaiAccount = new BankAccount(111, "Mezba", 20);
+
+mezbaBhaiAccount.userId = 222;
+
+mezbaBhaiAccount.addBalance(22);
+mezbaBhaiAccount.addBalance(58);
+
+console.log(mezbaBhaiAccount)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
