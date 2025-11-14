d6f78dc62696c8747cc2a22736fc1f742c1d94a0
ShuvoSW <shuvomajumder3369@gmail.com>
2025-11-14 16:53:32 +0600

    Function guard

--- M-7/SRC/instanceOfGuard.ts
+++ M-7/SRC/instanceOfGuard.ts
@@ -1,13 +1,13 @@
-//oop : instance iof type guard/ type narrowing
+// oop : instance iof type guard/ type narrowing
 
 class Person {
-    name: string; 
+    name: string;
 
     constructor(name: string) {
         this.name = name;
     }
 
-      getSleep(numOfhours: number) {
+    getSleep(numOfhours: number) {
         console.log(`${this.name} doinik ${numOfhours} ghonta ghumai`);
     }
 }
@@ -20,25 +20,32 @@ class Student extends Person {
     doStudy(numOfhours: number) {
         console.log(`${this.name} doinik ${numOfhours} ghonta study kore`);
     }
-
 }
 
 class Teacher extends Person {
     constructor(name: string) {
         super(name);
     }
-    
+
     takeClass(numOfhours: number) {
         console.log(`${this.name} doinik ${numOfhours} ghonta class ney`);
     }
+}
 
-    
+// function guard
+
+const isStudent = (user: Person) => {
+    return user instanceof Student; // user is student
+}
+
+const isTeacher = (user: Person) => {
+    return user instanceof Teacher;
 }
 
 const getUserInfo = (user: Person) => {
-    if (user instanceof Student) {
+    if (isStudent(user)) {
         user.doStudy(10);
-    } else if (user instanceof Teacher) {
+    } else if (isTeacher(user)) {
         user.takeClass(5);
     } else {
         user.getSleep(15);
@@ -46,9 +53,13 @@ const getUserInfo = (user: Person) => {
 }
 
 const student1 = new Student("Mr. student");
-const teacher1 = new Teacher("Mr. student");
+const teacher1 = new Teacher("Mr. Teacher");
+const person1 = new Person("Mr. Teacher");
 
-getUserInfo(student1)
+getUserInfo(student1);
+getUserInfo(teacher1);
+getUserInfo(person1);
+
 
 
 
