69789a03d1c9e0c25698d4850aa07209e817506e
ShuvoSW <shuvomajumder3369@gmail.com>
2025-11-20 18:09:37 +0600

    Inport

--- M-9/file_es-9-1.mjs
+++ M-9/file_es-9-1.mjs
@@ -1,14 +1,15 @@
-// commin js to esm
+// common js to esm
 // const { a } = require("./M09V08-2");
-
-const { a: x } = require("./M09V08-3");
+import { a }  from "./file_es-9-2.mjs";
+// const { a: x } = require("./M09V08-3");
 // const { add } = require("./Utils/add")
 // const { subs } = require("./Utils/subs.js");
 //  const {add, subs} = require("./utils")
 
 
 // console.log(x);
-// console.log(a);
+
 // console.log(a, x);
-console.log(add(2, 3));
-console.log(subs(x, a));
+// console.log(add(2, 3));
+// console.log(subs(x, a));
+console.log(a);
