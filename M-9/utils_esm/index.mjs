37ca218ce8118601c9c83e3589ad756db12bade2
ShuvoSW <shuvomajumder3369@gmail.com>
2025-11-20 18:43:51 +0600

    esm

--- M-9/utils_esm/index.mjs
+++ M-9/utils_esm/index.mjs
@@ -2,5 +2,9 @@ import {add} from "./add.mjs";
 // import subs from "./subs.mjs";
 import biyog from "./subs.mjs";
 
+export default {
+    add,
+    biyog
+}
 // console.log(subs(9, 6));
-console.log(biyog(9, 6));
+// console.log(biyog(9, 6));
