699e5d183c2be2fc1893ed6ec8cfe737c8854443
ShuvoSW <shuvomajumder3369@gmail.com>
2025-11-21 23:21:30 +0600

    Temp file deleted

--- nodejs_core_modules/delete-file.js
+++ nodejs_core_modules/delete-file.js
@@ -2,3 +2,12 @@ const fs = require("fs");
 
 fs.writeFileSync("./output/temp.txt", "this is a temp file");
 console.log("tem file created");
+
+// Temp file delete
+if(fs.existsSync("./output/temp.txt")) {
+    console.log("file exits!!!");
+
+    // file delete function
+    fs.unlinkSync("./output/temp.txt");
+    console.log("file deleted");
+}
