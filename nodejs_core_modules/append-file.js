e9b36f0c726f897612af6acacb9d55e19ba075b5
ShuvoSW <shuvomajumder3369@gmail.com>
2025-11-21 23:12:45 +0600

    Temp file creat

--- nodejs_core_modules/append-file.js
+++ nodejs_core_modules/append-file.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 fs.writeFileSync("./output/app.log", "Application Started\n");
 console.log("file created");
 
-const logEntry1 = `${new Date().toISOString()} user logged in\n`;
+const logEntry1 = `\n${new Date().toISOString()} user logged in\n`;
 fs.appendFileSync("./output/app.log", logEntry1);
 
 const logEntry2 = `${new Date().toISOString() } data fetched`;
---
+++ nodejs_core_modules/delete-file.js
@@ -0,0 +1,4 @@
+const fs = require("fs");
+
+fs.writeFileSync("./output/temp.txt", "this is a temp file");
+console.log("tem file created");
