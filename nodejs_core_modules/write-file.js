bca981e8b1e5eee1b1614d6dadd1a3fb9f1adf5c
ShuvoSW <shuvomajumder3369@gmail.com>
2025-11-21 21:36:49 +0600

    Write read async

---
+++ nodejs_core_modules/write-file.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+
+const content1 = " This is a content \n nodejs is awesome!!!";
+
+try{
+    fs.writeFileSync("./output/test-sync.txt", content1);
+    console.log("file written sync")
+} catch (err) {
+    console.err(err.message);
+}
