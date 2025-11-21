c3e4bc6f222e11c539630be0db9b68ffc2f21d1f
ShuvoSW <shuvomajumder3369@gmail.com>
2025-11-21 23:46:18 +0600

    Delet file asynchronouslly

--- nodejs_core_modules/delete-file.js
+++ nodejs_core_modules/delete-file.js
@@ -4,10 +4,28 @@ fs.writeFileSync("./output/temp.txt", "this is a temp file");
 console.log("tem file created");
 
 // Temp file delete
-if(fs.existsSync("./output/temp.txt")) {
+if (fs.existsSync("./output/temp.txt")) {
     console.log("file exits!!!");
 
     // file delete function
     fs.unlinkSync("./output/temp.txt");
     console.log("file deleted");
 }
+try {
+    fs.unlinkSync("./output/temp.txt");
+} catch (error) {
+    console.log("ERROR :", error.message);
+}
+
+fs.writeFile("./output/temp2.txt", "Another temp file", (err) => {
+    console.log("Another temp file created");
+
+    // asynchronuslly file delete
+    fs.unlink("./output/temp2.txt", (err) => {
+        if (err) {
+            console.error("Error :", err.message);
+        } else {
+            console.log("Temp2 deleted")
+        }
+    })
+})
