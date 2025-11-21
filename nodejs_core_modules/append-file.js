d6c3d815b78d8f588fd43bbe4cf223dd0c33247a
ShuvoSW <shuvomajumder3369@gmail.com>
2025-11-21 22:59:16 +0600

    Log entry2

--- nodejs_core_modules/append-file.js
+++ nodejs_core_modules/append-file.js
@@ -5,3 +5,8 @@ console.log("file created");
 
 const logEntry1 = `${new Date().toISOString()} user logged in\n`;
 fs.appendFileSync("./output/app.log", logEntry1);
+
+const logEntry2 = `${new Date().toISOString() } data fetched`;
+fs.appendFileSync("./output/app.log", logEntry2);
+
+console.log("task complete");
