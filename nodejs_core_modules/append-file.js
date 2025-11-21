d042ced77fc1fddfe2501d52333eba78018a338b
ShuvoSW <shuvomajumder3369@gmail.com>
2025-11-21 22:53:17 +0600

    Log entry

--- nodejs_core_modules/append-file.js
+++ nodejs_core_modules/append-file.js
@@ -2,3 +2,6 @@ const fs = require("fs");
 
 fs.writeFileSync("./output/app.log", "Application Started\n");
 console.log("file created");
+
+const logEntry1 = `${new Date().toISOString()} user logged in\n`;
+fs.appendFileSync("./output/app.log", logEntry1);
