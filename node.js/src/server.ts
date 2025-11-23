afcdba7dba397200fb744630951fd2baa40c7a71
ShuvoSW <shuvomajumder3369@gmail.com>
2025-11-23 23:16:12 +0600

    Post

--- node.js/src/server.ts
+++ node.js/src/server.ts
@@ -1,21 +1,45 @@
 import http, { IncomingMessage, Server, ServerResponse } from "http";
+import config from "./config";
 
 const server: Server = http.createServer(
     (req: IncomingMessage, res: ServerResponse) => {
-    console.log("server is sunning...");
+        console.log("server is sunning...");
 
-    if (req.url == "/" && req.method == "GET") {
-        res.writeHead(200, { "content-type": "application/json"});
-        res.end(
-            JSON.stringify({
-                message: "Hello from node js with typescript...",
-                path: req.url,
-            })
-        )
-    }
-})
+        // root rout
+        if (req.url == "/" && req.method == "GET") {
+            res.writeHead(200, { "content-type": "application/json" });
+            res.end(
+                JSON.stringify({
+                    message: "Hello from node js with typescript...",
+                    path: req.url,
+                })
+            )
+        }
+        // health rout
+        if (req.url == "/api" && req.method == "GET") {
+            res.writeHead(200, { "content-type": "application/json" });
+            res.end(
+                JSON.stringify({
+                    message: "Health status ok",
+                    path: req.url,
+                })
+            )
+        }
 
 
-server.listen(5000, () => {
-    console.log(`server is running on port ${5000}`);
+        if (req.url == "/api/user" && req.method == "POST") {
+            const user = {
+                id: 1,
+                name: "alice"
+            }
+            res.writeHead(200, { "content-type": "application/json" });
+            res.end(
+                JSON.stringify(user)
+            )
+        }
+    })
+
+
+server.listen(config.port, () => {
+    console.log(`server is running on port ${config.port}`);
 })
