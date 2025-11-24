87498c80157df646a05f1d4949a515cfb3071f08
ShuvoSW <shuvomajumder3369@gmail.com>
2025-11-24 21:43:27 +0600

    Coustom route handler

--- node.js/src/server.ts
+++ node.js/src/server.ts
@@ -1,30 +1,61 @@
 import http, { IncomingMessage, Server, ServerResponse } from "http";
 import config from "./config";
+import addRoutes, { RouteHandler, routes } from "./helpers/RouteHandler";
+
+addRoutes("GET", "/", (req, res) => {
+    res.writeHead(200, { "content-type": "application/json" });
+    res.end(
+        JSON.stringify({
+            message: "Hello from node js with typescript...",
+            path: req.url,
+        })
+    )
+})
 
 const server: Server = http.createServer(
     (req: IncomingMessage, res: ServerResponse) => {
         console.log("server is sunning...");
 
+        const method = req.method?.toUpperCase() || "";
+
+        const path = req.url || "";
+
+        const methodMap = routes.get(method);
+
+        const handler: RouteHandler | undefined = methodMap?.get(path);
+
+        if (handler) {
+            handler(req, res);
+        } else {
+            res.writeHead(404, { "content-type": "application/json" });
+            res.end(
+                JSON.stringify({
+                    success: false,
+                    message: "Route not found!!!",
+                    path,
+                })
+            )
+        }
         // root rout
-        if (req.url == "/" && req.method == "GET") {
-            res.writeHead(200, { "content-type": "application/json" });
-            res.end(
-                JSON.stringify({
-                    message: "Hello from node js with typescript...",
-                    path: req.url,
-                })
-            )
-        }
+        // if (req.url == "/" && req.method == "GET") {
+        //     res.writeHead(200, { "content-type": "application/json" });
+        //     res.end(
+        //         JSON.stringify({
+        //             message: "Hello from node js with typescript...",
+        //             path: req.url,
+        //         })
+        //     )
+        // }
         // health rout
-        if (req.url == "/api" && req.method == "GET") {
-            res.writeHead(200, { "content-type": "application/json" });
-            res.end(
-                JSON.stringify({
-                    message: "Health status ok",
-                    path: req.url,
-                })
-            )
-        }
+        // if (req.url == "/api" && req.method == "GET") {
+        // res.writeHead(200, { "content-type": "application/json" });
+        // res.end(
+        //     JSON.stringify({
+        //         message: "Health status ok",
+        //         path: req.url,
+        //     })
+        // )
+        // }
 
 
         // if (req.url == "/api/user" && req.method == "POST") {
@@ -36,35 +67,36 @@ const server: Server = http.createServer(
         //     res.end(
         //         JSON.stringify(user)
         //     )
-        if (req.url == "/api/user" && req.method == "POST") {
-            let body = '';
+        // if (req.url == "/api/user" && req.method == "POST") {
+        // let body = '';
 
-            //listen for data chunk
-            req.on("data", (chunk) => {
-                body += chunk.toString();
-            });
+        // //listen for data chunk
+        // req.on("data", (chunk) => {
+        //     body += chunk.toString();
+        // });
 
-            req.on("end", () => {
-                try {
-                    const parseBody = JSON.parse(body);
-                    // console.log(body);
-                    console.log(parseBody);
-                    // res.end(body);
-                    console.log("catching current changes");
-                    res.end(JSON.stringify(parseBody));
-                } catch (err: any) {
-                    console.log(err.message);
-                }
-            })
+        // req.on("end", () => {
+        //     try {
+        //         const parseBody = JSON.parse(body);
+        //         // console.log(body);
+        //         console.log(parseBody);
+        //         // res.end(body);
+        //         console.log("catching current changes");
+        //         res.end(JSON.stringify(parseBody));
+        //     } catch (err: any) {
+        //         console.log(err.message);
+        //     }
+        // })
 
 
-            // res.end(JSON.stringify({
-            //     message: "processing...",
-            // }))
-        }
-    })
+        // res.end(JSON.stringify({
+        //     message: "processing...",
+        // }))
+    }
+    // }
+)
 
 
 server.listen(config.port, () => {
     console.log(`server is running on port ${config.port}`);
-})
+})   
