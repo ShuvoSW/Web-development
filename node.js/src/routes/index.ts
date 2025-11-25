e64f5fac643351caea21c7a41f4d1daa5b2aeabc
ShuvoSW <shuvomajumder3369@gmail.com>
2025-11-25 20:00:29 +0600

    Setting fake json database

--- node.js/src/routes/index.ts
+++ node.js/src/routes/index.ts
@@ -1,4 +1,4 @@
-import { readUsers } from "../helpers/fileDb";
+import { readUsers, writeUsers } from "../helpers/fileDb";
 import parseBody from "../helpers/parseBody";
 import addRoutes from "../helpers/RouteHandler";
 import sendJson from "../helpers/sendJson";
@@ -21,7 +21,17 @@ addRoutes("GET", "/api", (req, res) => {
 addRoutes("POST", "/api/users", async(req, res) => {
     const body = await parseBody(req);
     
+    // user json read
     const users = readUsers()
+
+    const newUser = {
+        id: Date.now(),
+        ...body,
+    };
+
+    users.Push(newUser);
+
+    writeUsers(users);
 
     sendJson(res, 201, { success: true, data: body})
 })
