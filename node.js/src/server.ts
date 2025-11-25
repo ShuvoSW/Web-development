364f1d48b7494e4310fc329eb303d31589aae0e4
ShuvoSW <shuvomajumder3369@gmail.com>
2025-11-25 20:47:17 +0600

    FindDynamicRoute for loop add

--- node.js/src/server.ts
+++ node.js/src/server.ts
@@ -8,7 +8,23 @@ function findDynamicRoute(method: string,url: string) {
     if(!methodMap) return null;
 
     for(const [routePath, handler] of methodMap.entries()){
-        
+        const routeParts = routePath.split("/");
+        const urlParts = url.split("/")
+
+        if(routeParts.length !== urlParts.length) continue;
+
+        const params: any ={};
+        let matched = true
+
+        // "/api/users/:id"
+        for(let i = 0; i<routeParts.length; i++) {
+            if(routeParts[i]?.startsWith(":")) {
+                params[routeParts[i]?.substring(1)!] = urlParts[i];
+            } else if(routeParts[i] !== urlParts[i]) {
+                matched = false;
+                
+            }
+        }
     }
 }
 
