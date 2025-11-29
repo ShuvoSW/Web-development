a653fda1f25081e54dc04e9338e52883bff09a14
ShuvoSW <shuvomajumder3369@gmail.com>
2025-11-29 22:21:04 +0600

    Postgres

--- server_with_express/src/server.ts
+++ server_with_express/src/server.ts
@@ -1,7 +1,15 @@
 import express, { Request, Response } from "express"
+import {Pool} from "pg"
 // const express = require('express')
+
 const app = express()
 const port = 5000
+
+const pool = new Pool({
+  connectionString: `postgresql://neondb_owner:npg_3SZgd0xytJVp@ep-morning-violet-a4q1knec-pooler.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require`
+})
+
+
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello next level developers')
