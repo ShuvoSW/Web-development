74712127e9e5c0963ee5495cbe20b67fa5af53de
ShuvoSW <shuvomajumder3369@gmail.com>
2025-11-29 21:22:11 +0600

    Post method

--- server_with_express/src/server.ts
+++ server_with_express/src/server.ts
@@ -1,1 +1,21 @@
 import express, { Request, Response } from "express"
+// const express = require('express')
+const app = express()
+const port = 5000
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello next level developers')
+})
+
+app.post("/", (req: Request, res: Response) => {
+    console.log(req.body);
+
+    res.status(201).json({
+            success: true,
+            message: "API is working"
+    })
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
