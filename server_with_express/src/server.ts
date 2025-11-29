7890d4dabd6b5e65e5db330822da4bb8145a2edf
ShuvoSW <shuvomajumder3369@gmail.com>
2025-11-29 23:20:04 +0600

    Add .env

--- server_with_express/src/server.ts
+++ server_with_express/src/server.ts
@@ -1,6 +1,10 @@
 import express, { Request, Response } from "express"
 import { Pool } from "pg"
+import dotenv from "dotenv";
+import path from "path";
 // const express = require('express')
+
+dotenv.config({path: path.join(process.cwd(), ".env")});
 
 const app = express()
 const port = 5000
@@ -10,10 +14,10 @@ app.use(express.json());
 
 //DB
 const pool = new Pool({
-  connectionString: `postgresql://neondb_owner:npg_3SZgd0xytJVp@ep-morning-violet-a4q1knec-pooler.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require`
+  connectionString: `${process.env.CONNECTION_STR}`
 })
 
-const initDB = async() => {
+const initDB = async () => {
   await pool.query(`
     CREATE TABLE IF NOT EXISTS users(
     id SERIAL PRIMARY KEY,
@@ -23,9 +27,22 @@ const initDB = async() => {
     phone VARCHAR(15),
     address TEXT,
     created_at TIMESTAMP DEFAULT NOW(),
-    updated_at TIMESTAMP DEFAULT NOW(),
+    updated_at TIMESTAMP DEFAULT NOW()
     )
     `)
+
+  await pool.query(`
+      CREATE TABLE IF NOT EXISTS todos(
+      id SERIAL PRIMARY KEY,
+      user_id INT REFERENCES users(id) ON DELETE CASCADE,
+      title VARCHAR(200) NOT NULL,
+      description TEXT,
+      completed BOOLEAN DEFAULT false,
+      due_date DATE,
+      created_at TIMESTAMP DEFAULT NOW(),
+      updated_at TIMESTAMP DEFAULT NOW()
+      ) 
+      `)
 }
 
 initDB()
