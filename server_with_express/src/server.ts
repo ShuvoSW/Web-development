48840d149d2509f321bf1c8d781740bcfab8ef22
ShuvoSW <shuvomajumder3369@gmail.com>
2025-11-30 20:49:35 +0600

    Error

--- server_with_express/src/server.ts
+++ server_with_express/src/server.ts
@@ -222,6 +222,15 @@ app.get("/todos", async (req: Request, res: Response) => {
   }
 })
 
+// error
+app.use((req, res) => {
+  res.status(404).json({
+    success: false,
+    message: "Route not found",
+    path: req.path,
+  });
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
