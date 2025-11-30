5835f1489aa493b74ee67593ab796ece5c724f00
ShuvoSW <shuvomajumder3369@gmail.com>
2025-11-30 18:04:48 +0600

    User using post

--- server_with_express/src/server.ts
+++ server_with_express/src/server.ts
@@ -53,15 +53,20 @@ app.get('/', (req: Request, res: Response) => {
 
 app.post("/users", async (req: Request, res: Response) => {
   // console.log(req.body);
-  // console.log(req.body);
   const {name, email} = req.body
 
   try{
     const result = await pool.query(`INSERT INTO users(name, email) VALUES($1, $2) RETURNING *`, [name, email]);
-    console.log(result);
+    // console.log(result.rows[0]);
 
-    res.send({message: "data inserted"})
+    // res.send({message: "data inserted"})
 
+      res.status(201).json({
+      success: false,
+      message: "Data Inserted Successfully",
+      data: result.rows[0]
+    })
+    
   }catch (err: any) {
     res.status(500).json({
       success: false,
