e5713d934be66d37fd591b352483ae0fee9e0bf9
ShuvoSW <shuvomajumder3369@gmail.com>
2025-11-30 19:25:16 +0600

    User delete

--- server_with_express/src/server.ts
+++ server_with_express/src/server.ts
@@ -155,6 +155,33 @@ app.put("/users/:id", async (req: Request, res: Response) => {
   }
 })
 
+// delete
+app.delete("/users/:id", async (req: Request, res: Response) => {
+  // console.log(req.params.id);
+  // res.send({ message: "API is coll..."})
+  try{
+    const result = await pool.query(`DELETE FROM users WHERE id = $1`, [req.params.id])
+    if(result.rows.length === 0){
+      res.status(404).json({
+        success: false,
+        message: "User not found",
+      })
+    }else {
+      res.status(200).json({
+        success: true,
+      message: "User fetched successfully",
+      data: null
+      })
+    }
+    // console.log(result.rows);
+  }catch(err: any) {
+    res.status(500).json({
+      success: false,
+      message: err.message
+    })
+  }
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
