9fb3ea99ffe3b87dca39911d0a374ca4923f519f
ShuvoSW <shuvomajumder3369@gmail.com>
2025-12-01 22:16:08 +0600

    user.controller.ts

---
+++ server_with_express 12, 13/src/modules/user/user.controller.ts
@@ -0,0 +1,35 @@
+import { Request, Response } from "express";
+import { pool } from "../../config/db";
+
+const createUser = async (req: Request, res: Response) => {
+    // console.log(req.body);
+    const { name, email } = req.body
+
+    try {
+        const result = await pool.query(`INSERT INTO users(name, email) VALUES($1, $2) RETURNING *`, [name, email]);
+        // console.log(result.rows[0]);
+
+        // res.send({message: "data inserted"})
+
+        res.status(201).json({
+            success: false,
+            message: "Data Inserted Successfully",
+            data: result.rows[0]
+        })
+
+    } catch (err: any) {
+        res.status(500).json({
+            success: false,
+            message: err.message
+        })
+    }
+
+    // res.status(201).json({
+    //   success: true,
+    //   message: "API is working"
+    // })
+}
+
+export const userControllers = {
+    createUser,
+};
--- server_with_express 12, 13/src/modules/user/user.routes.ts
+++ server_with_express 12, 13/src/modules/user/user.routes.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from "express";
 import { pool } from "../../config/db";
+import { userControllers } from "./user.controller";
 
 const router = express.Router()
 
@@ -7,34 +8,7 @@ const router = express.Router()
 
 // route -> controller -> service
 
-router.post("/", async (req: Request, res: Response) => {
-  // console.log(req.body);
-  const { name, email } = req.body
-
-  try {
-    const result = await pool.query(`INSERT INTO users(name, email) VALUES($1, $2) RETURNING *`, [name, email]);
-    // console.log(result.rows[0]);
-
-    // res.send({message: "data inserted"})
-
-    res.status(201).json({
-      success: false,
-      message: "Data Inserted Successfully",
-      data: result.rows[0]
-    })
-
-  } catch (err: any) {
-    res.status(500).json({
-      success: false,
-      message: err.message
-    })
-  }
-
-  // res.status(201).json({
-  //   success: true,
-  //   message: "API is working"
-  // })
-});
+router.post("/", userControllers.createUser);
 
 router.get("/",  async (req: Request, res: Response) => {
   try {
