5a6ddd144a585c1ed7323d18a437b5a544703ada
ShuvoSW <shuvomajumder3369@gmail.com>
2025-12-02 23:32:42 +0600

    Role based authentication

--- server_with_express 12, 13/src/middleware/auth.ts
+++ server_with_express 12, 13/src/middleware/auth.ts
@@ -4,7 +4,10 @@ import { NextFunction, Request, Response } from "express"
 import jwt, { JwtPayload } from "jsonwebtoken";
 import config from "../config";
 
-const auth = () => {
+
+
+// roles = ["admin", "user"]
+const auth = (...roles: string[]) => {
     return async (req: Request, res: Response, next: NextFunction) => {
 
 
@@ -14,9 +17,18 @@ const auth = () => {
             if (!token) {
                 return res.status(500).json({ message: "You are not allowed!!" });
             }
-            const decode = jwt.verify(token, config.jwtSecret as string);
-            console.log({ decode });
-            req.user = decode as JwtPayload;
+            const decoded = jwt.verify(token, config.jwtSecret as string) as JwtPayload;
+            console.log({ decoded });
+            req.user = decoded;
+            
+            ["admin"]
+            if(roles.length && !roles.includes(decoded.role as string)){
+                return res.status(500).json({
+                     error: "unauthorized!!!",
+                })
+                
+            }
+
             next();
         } catch (err: any) {
             res.status(500).json({
