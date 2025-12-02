d01a8a1b9657f1ae25b3484b6f6a6274df239a83
ShuvoSW <shuvomajumder3369@gmail.com>
2025-12-02 22:41:18 +0600

    Auth middleware

--- server_with_express 12, 13/src/modules/user/user.routes.ts
+++ server_with_express 12, 13/src/modules/user/user.routes.ts
@@ -2,6 +2,7 @@ import express, { Request, Response } from "express";
 import { pool } from "../../config/db";
 import { userControllers } from "./user.controller";
 import logger from "../../middleware/logger";
+import auth from "../../middleware/auth";
 
 const router = express.Router()
 
@@ -11,7 +12,7 @@ const router = express.Router()
 
 router.post("/", userControllers.createUser);
 
-router.get("/", logger, userControllers.getUser);
+router.get("/", logger, auth(), userControllers.getUser);
 
 router.get("/:id",userControllers.getSingleUser);
 
