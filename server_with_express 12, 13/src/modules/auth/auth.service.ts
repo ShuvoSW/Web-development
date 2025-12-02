324bc0c62031dcdb0b4958ca75a15efbdb7be84b
ShuvoSW <shuvomajumder3369@gmail.com>
2025-12-02 20:25:14 +0600

    Bcrypt comparison

--- server_with_express 12, 13/src/modules/auth/auth.service.ts
+++ server_with_express 12, 13/src/modules/auth/auth.service.ts
@@ -1,10 +1,19 @@
-import { Router } from "express";
+import { pool } from "../../config/db"
+import bcrypt from "bcryptjs";
 
-const router = Router();
+const loginUser = async (email: string, password: string) => {
+    const result = await pool.query(`SELECT * FROM users WHERE email=$1`, [email]);
+    if (result.rows.length === 0) {
+        return null;
+    }
+    const user = result.rows[0];
 
+    const match = await bcrypt.compare(password, user.password);
 
-
-export const authRoutes = router
+    if(!match){
+        return false
+    }
+}
 
 
 
