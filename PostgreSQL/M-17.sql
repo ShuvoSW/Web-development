825b7e510cf9f9541a449e1a76fa75986b8a414b
ShuvoSW <shuvomajumder3369@gmail.com>
2025-12-13 20:44:17 +0600

    Create table

--- PostgreSQL/M-17.sql
+++ PostgreSQL/M-17.sql
@@ -3,3 +3,12 @@ create database school;
 
 -- Delate database
 drop database school;
+
+-- Create table
+create table students (
+  id serial,
+  name varchar(50),
+  age int,
+  isActive boolean,
+  dob date
+)
