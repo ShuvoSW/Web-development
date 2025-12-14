f7c9205f4725591178b37661b0ce47a844210d4f
ShuvoSW <shuvomajumder3369@gmail.com>
2025-12-14 21:26:17 +0600

    Set and remove default value, add and drop table level constraint

--- PostgreSQL/M-18.sql
+++ PostgreSQL/M-18.sql
@@ -44,3 +44,24 @@ alter column email set not null;
 -- Drop constraint
 alter table employee
 alter column email drop not null;
+
+-- Set default value
+insert into employee (user_name, age) values ('shuvo', 25)
+alter table employee
+
+alter column email set default 'test@email.com'
+
+-- Remove default value
+alter table employee
+alter column email drop default;
+
+-- Add constraint (table level constraint)
+alter table employee
+  add constraint unique_employee_email unique(email);
+
+alter table employee
+  add constraint pk_employee_id primary key(id);  
+
+-- Drop constraint (table level constraint)
+alter table employee
+  drop constraint unique_employee_email;  
