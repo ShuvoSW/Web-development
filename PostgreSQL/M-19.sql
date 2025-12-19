0f4f33726a1d7c3bcf32259b8c2c91c008e30510
ShuvoSW <shuvomajumder3369@gmail.com>
2025-12-19 22:15:45 +0600

    Full, Cross, Natural join

--- PostgreSQL/M-19.sql
+++ PostgreSQL/M-19.sql
@@ -116,7 +116,46 @@ inner join posts as p on p.user_id = u.id;
 select * from posts as p
 left join users as u on p.user_id = u.id;
 
+select * from posts as p
+left outer join users as u on p.user_id = u.id;
+
 
 -- Right join
 select * from posts as p
 right join users as u on p.user_id = u.id;
+
+select * from posts as p
+right outer join users as u on p.user_id = u.id;
+
+-- Full join
+select * from posts as p
+full join users as u on p.use
+
+
+-- Cross join
+CREATE TABLE  employees (
+  emp_id INT,
+  emp_name VARCHAR(50),
+  dept_id INT
+);
+
+CREATE TABLE departments (
+  dept_id INT,
+  dept_name VARCHAR(50)
+);
+
+INSERT INTO employees VALUES (1, 'John Doe', 101);
+INSERT INTO employees VALUES (2, 'jane smith', 102);
+
+INSERT INTO departments VALUES (101, 'Human Resources');
+INSERT INTO departments VALUES (102, 'Marketing');
+
+SELECT * from employees;
+SELECT * from departments;
+-- Main cross join 
+select * from employees
+cross join departments;
+
+-- Natural join
+select * from employees
+natural join departments;
