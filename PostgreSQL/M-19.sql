0b48e0449bdabe4237b29d91904d9b3992943237
ShuvoSW <shuvomajumder3369@gmail.com>
2025-12-19 18:48:54 +0600

    Updating Data

--- PostgreSQL/M-19.sql
+++ PostgreSQL/M-19.sql
@@ -13,10 +13,22 @@ select first_name, age, coalesce(email, 'Not Provided') as email from students;
 
 
 -- LIMIT, OFFSET Pagination
-select * from students limit 2
+select * from students limit 2;
   
-select * from students limit 2 offset 1
+select * from students limit 2 offset 1;
   
-select * from students limit 2 offset 2 * 0
-select * from students limit 2 offset 2 * 1
-select * from students limit 2 offset 2 * 2
+select * from students limit 2 offset 2 * 0;
+select * from students limit 2 offset 2 * 1;
+select * from students limit 2 offset 2 * 2;
+
+
+-- Updating Data
+select * from students; 
+
+update students set email = 'default@gmail.com' where email is null;
+
+update students set email = 'default@gmail.com' where email like 'a%';
+
+update students set first_name = 'Pronoy', age = 25 where student_id = 1
+  
+update students set grade = 'C' where student_id in (1, 3);
