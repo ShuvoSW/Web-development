38c60991c2757314aacccf71a906384576796c02
ShuvoSW <shuvomajumder3369@gmail.com>
2025-12-20 19:58:26 +0600

    Postgres Functions

--- PostgreSQL/M-20.sql
+++ PostgreSQL/M-20.sql
@@ -110,3 +110,25 @@ where salary = (
   select max(salary) from employees
   where department = 'HR'
 );
+
+
+-- Postgres Functions
+create function emp_count()
+returns int
+language sql
+as 
+$$
+  select count(*) from employees
+$$;
+  
+select emp_count();
+
+create function delete_emp_id(emp_id int)
+returns void
+language  sql
+as 
+$$
+ delete from employees where id = emp_id  
+$$;
+
+select delete_emp_id(5);
