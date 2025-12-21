21706687d7f14791271ec51100f8ac7d95acb940
ShuvoSW <shuvomajumder3369@gmail.com>
2025-12-21 22:02:03 +0600

    Users data insert

--- Vehicle Rental System - SQL Queries/queries.sql
+++ Vehicle Rental System - SQL Queries/queries.sql
@@ -1,11 +1,28 @@
 CREATE TYPE
   user_role AS ENUM('Customer', 'Admin');
-  
+
 CREATE TABLE
   users (
     user_id SERIAL PRIMARY KEY,
-    name VARCHAR(50) not null,
+    user_name VARCHAR(50) not null,
     email VARCHAR(50) unique not null,
     phone VARCHAR(20) not null,
     role user_role NOT NULL
+  );
+
+INSERT INTO
+  users (user_name, email, phone, role)
+values
+  (
+    'Alice',
+    'alice@example.com',
+    '1234567890',
+    'Customer'
+  ),
+  ('Bob', 'bob@example.com', '0987654321', 'Admin'),
+  (
+    'Charlie',
+    'charlie@example.com',
+    '1122334455',
+    'Customer'
   );
