bbf87a95e11c1aff8b2ea112807925a439a16427
ShuvoSW <shuvomajumder3369@gmail.com>
2025-12-27 15:28:44 +0600

    Omit<>

--- prisma-blog-app-23/src/modules/post/post.controller.ts
+++ prisma-blog-app-23/src/modules/post/post.controller.ts
@@ -1,8 +1,13 @@
 import { Request, Response } from "express"
+import { postService } from "./post.service"
+import { Post } from "../../../generated/prisma/client"
 
 const createPost = (req: Request, res: Response) => {
     // res.send("Create a new post")
-    console.log({ req, res });
+    // console.log({ req, res });
+    try{
+        const result = await postService.createPost(req.body as Omit<Post, "id" | "createdAt" | "updatedAt">)
+    }
 }
 
 export const postController = {
