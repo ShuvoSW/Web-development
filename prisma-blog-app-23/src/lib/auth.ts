e1c156b60547ab812d0d7fdb00fd85cd416ba93f
ShuvoSW <shuvomajumder3369@gmail.com>
2025-12-28 23:09:52 +0600

    emailAndPassword: {     enabled: true,   },

--- prisma-blog-app-23/src/lib/auth.ts
+++ prisma-blog-app-23/src/lib/auth.ts
@@ -6,4 +6,7 @@ export const auth = betterAuth({
     database: prismaAdapter(prisma, {
         provider: "sqlite", // or "mysql", "postgresql", ...etc
     }),
+      emailAndPassword: { 
+    enabled: true, 
+  }, 
 });
