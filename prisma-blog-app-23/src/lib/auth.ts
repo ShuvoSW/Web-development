9ae96a54b0bbefdaf72aea1f6f06abc59f7d7bce
ShuvoSW <shuvomajumder3369@gmail.com>
2025-12-28 22:59:30 +0600

    npx @better-auth/cli generate

--- prisma-blog-app-23/src/lib/auth.ts
+++ prisma-blog-app-23/src/lib/auth.ts
@@ -4,6 +4,6 @@ import { prisma } from "./prisma";
 
 export const auth = betterAuth({
     database: prismaAdapter(prisma, {
-        provider: "postgresql", // or "mysql", "postgresql", ...etc
+        provider: "sqlite", // or "mysql", "postgresql", ...etc
     }),
 });
