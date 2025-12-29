c24857f616618ea302a7f73492e8a9fd2e5cfe54
ShuvoSW <shuvomajumder3369@gmail.com>
2025-12-29 20:33:33 +0600

    cors, emailVerification

--- prisma-blog-app-23/src/lib/auth.ts
+++ prisma-blog-app-23/src/lib/auth.ts
@@ -27,7 +27,14 @@ export const auth = betterAuth({
     },
     emailAndPassword: {
         enabled: true,
+        autoSignIn: false,
+        requireEmailVerification: true
     },
+    emailVerification: {
+    sendVerificationEmail: async ( { user, url, token }, request) => {
+     console.log("****** Verification email send");
+    },
+  },
 });
 
 
