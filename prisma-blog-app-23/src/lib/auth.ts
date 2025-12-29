9971e77931ba4c7c8ba16b690a9261c46b4b40e5
ShuvoSW <shuvomajumder3369@gmail.com>
2025-12-29 22:45:39 +0600

    autoSignInAfterVerification, prompt, accessType

--- prisma-blog-app-23/src/lib/auth.ts
+++ prisma-blog-app-23/src/lib/auth.ts
@@ -43,6 +43,7 @@ export const auth = betterAuth({
     },
     emailVerification: {
         sendOnSignUp: true,
+        autoSignInAfterVerification: true,
         sendVerificationEmail: async ({ user, url, token }, request) => {
             // console.log(user, url, token);
             try {
@@ -140,6 +141,17 @@ export const auth = betterAuth({
             }
         },
     },
+
+        socialProviders: {
+        google: { 
+            prompt: "select_account consent",
+            accessType: "offline",
+            clientId: process.env.GOOGLE_CLIENT_ID as string, 
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string, 
+        }, 
+    },
+
+    
 });
 
 
