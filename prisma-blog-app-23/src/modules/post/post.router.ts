c881071b22aa96cda6f1643154a460e5783dfb8a
ShuvoSW <shuvomajumder3369@gmail.com>
2025-12-30 18:47:56 +0600

    NextFunction

--- prisma-blog-app-23/src/modules/post/post.router.ts
+++ prisma-blog-app-23/src/modules/post/post.router.ts
@@ -1,14 +1,18 @@
-import express, { Router } from 'express';
+import express, { NextFunction, Request, Response, Router } from 'express';
 import { postController } from './post.controller';
 
 const router = express.Router();
 
 const auth=()=> {
-
+    return async (req: Request,res: Response, next: NextFunction) => {
+        console.log("middleware!!!!!");
+        next()
+    }
 }
 
 router.post(
     "/",
+    auth(),
      postController.createPost
 )
 
