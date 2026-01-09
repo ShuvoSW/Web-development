50ea56c58a68bb2523d76a100312b28ab4131f8c
ShuvoSW <shuvomajumder3369@gmail.com>
2026-01-09 17:58:48 +0600

    Twofactor add

--- Milestone -5/cs-m-5-1-&-2/src/lib/auth.ts
+++ Milestone -5/cs-m-5-1-&-2/src/lib/auth.ts
@@ -1,8 +1,11 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { prisma } from "./prisma";
+import { twoFactor } from "better-auth/plugins";
 
 export const auth = betterAuth({
+
+    appName: "lab log",
 
     database: prismaAdapter(prisma, {
         provider: "postgresql",
@@ -24,7 +27,11 @@ export const auth = betterAuth({
             clientSecret:process.env.GITHUB_CLIENT_SECRET as string,
             redirectURI:`${process.env.FRONTEND_URL}/api/auth/callback/github`
         }
-    }
+    },
+
+
+    plugins:[twoFactor()],
+
 })
 
 //   advanced: { disableOriginCheck: true }
