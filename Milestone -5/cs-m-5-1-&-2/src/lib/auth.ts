e42cc25d32000a796665ae4aeb87db725bfbc8aa
ShuvoSW <shuvomajumder3369@gmail.com>
2026-01-09 15:46:12 +0600

    User sign out

---
+++ Milestone -5/cs-m-5-1-&-2/src/lib/auth.ts
@@ -0,0 +1,24 @@
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+import { prisma } from "./prisma";
+
+export const auth = betterAuth({
+
+    database: prismaAdapter(prisma, {
+        provider: "postgresql",
+    }),
+
+    trustedOrigins: [process.env.FRONTEND_URL!],
+
+    emailAndPassword: {
+        enabled: true,
+        minPasswordLength: 6
+        // password: {
+            
+        // }
+    }
+
+
+})
+
+//   advanced: { disableOriginCheck: true }
