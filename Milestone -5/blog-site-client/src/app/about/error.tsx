07f76310b14cb2f42fd43fd6163ef26c6958518b
ShuvoSW <shuvomajumder3369@gmail.com>
2026-01-13 20:36:00 +0600

    Error.tsx

--- Milestone -5/blog-site-client/src/app/about/error.tsx
+++ Milestone -5/blog-site-client/src/app/about/error.tsx
@@ -1,8 +1,19 @@
 "use client";
-export default function AboutError() {
+
+import { useEffect } from "react";
+
+export default function AboutError({error, reset}: {
+    error: Error & {digest?: string};
+    reset: () => void;
+}) {
+    useEffect(() => {
+        // We can pass this error to a logger
+        console.error(error);
+    }, []);
   return (
     <div>
-      <h1>Something Went Wrong: Please try again later</h1>
+      <h1>Something Went Wrong: Please try again</h1>
+      <button onClick={() => reset()}>Retry</button>
     </div>
   );
 }
