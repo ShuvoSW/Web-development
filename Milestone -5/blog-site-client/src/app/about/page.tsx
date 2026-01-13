f435bf5ae9aa94f78696309fce93ab35b6e910a7
ShuvoSW <shuvomajumder3369@gmail.com>
2026-01-13 23:29:42 +0600

    Dashboard layout.ts

--- Milestone -5/blog-site-client/src/app/about/page.tsx
+++ Milestone -5/blog-site-client/src/app/about/page.tsx
@@ -1,7 +1,11 @@
+
+export const dynamic = 'force-dynamic';
 export default async function AboutPage() {
+  //For this simulation load time
   await new Promise((resolve) => setTimeout(resolve, 4000));
 
-    throw new Error("Something went wrong");
+    //For this simulation error
+    // throw new Error("Something went wrong");
   return (
     <div>
       <h1>This is about page component</h1>
