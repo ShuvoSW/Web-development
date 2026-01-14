d8a5a7d355e21354875a2589ad1433653d5e6349
ShuvoSW <shuvomajumder3369@gmail.com>
2026-01-14 20:01:45 +0600

    Analytics & write blog

---
+++ Milestone -5/blog-site-client/src/app/(dashboardLayout)/dashboard/analytics/page.tsx
@@ -0,0 +1,7 @@
+export default function Analytics() {
+  return (
+    <div>
+      <h1>This is Analytics</h1>
+    </div>
+  );
+}
--- Milestone -5/blog-site-client/src/components/layout/Navbar.tsx
+++ Milestone -5/blog-site-client/src/components/layout/Navbar.tsx
@@ -138,6 +138,10 @@ const Navbar = ({
       title: "About",
       url: "/about",
     },
+    {
+      title: "Dashboard",
+      url: "/dashboard",
+    },
   ],
   auth = {
     login: { title: "Login", url: "/login" },
