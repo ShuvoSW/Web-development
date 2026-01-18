8e77278a0f687897b561ad9558de71ed088c726f
ShuvoSW <shuvomajumder3369@gmail.com>
2026-01-18 22:22:14 +0600

    create a service layer

--- Mission-5/blog-site-client-30--31/src/app/(commonLayout)/page.tsx
+++ Mission-5/blog-site-client-30--31/src/app/(commonLayout)/page.tsx
@@ -1,29 +1,32 @@
 import { Button } from "@/components/ui/button";
-import { cookies } from "next/headers";
+import { userService } from "@/services/user.service";
+
 // import { authClient } from "@/lib/auth.client";
 
 export default async function Home() {
   // const session = await authClient.getSession();
   // console.log(session);
 
-  const cookieStore = await cookies()
-  console.log(cookieStore);
-  // console.log(cookieStore.get("better-auth.session_token"));
-  // cookieStore.set("test", "value") // no get server component
+  // const cookieStore = await cookies()
+  // console.log(cookieStore);
+  // // console.log(cookieStore.get("better-auth.session_token"));
+  // // cookieStore.set("test", "value") // no get server component
 
-  console.log(cookieStore.toString());
+  // console.log(cookieStore.toString());
 
-  const res = await fetch("http://localhost:5000/api/auth/get-session", {
-    headers: {
-      cookie: cookieStore.toString()
-    },
-    cache: "no-cache",
-  });
-  // console.log(await res.json())
+  // const res = await fetch("http://localhost:5000/api/auth/get-session", {
+  //   headers: {
+  //     cookie: cookieStore.toString()
+  //   },
+  //   cache: "no-cache",
+  // });
+  // // console.log(await res.json())
 
-  const session = await res.json()
-  console.log(session);
+  // const session = await res.json()
+  // console.log(session);
 
+const {data} = await userService.getSession();
+console.log(data);
   return (
     <div>
       <Button variant="outline">Click Here</Button>
