67575f15b141283f8756656d3647070afd8ffa9f
ShuvoSW <shuvomajumder3369@gmail.com>
2026-01-19 20:33:22 +0600

    ISR

--- Mission-5/blog-site-client-30--31/src/services/blog.service.ts
+++ Mission-5/blog-site-client-30--31/src/services/blog.service.ts
@@ -2,12 +2,15 @@ import { env } from "@/env"
 
 const API_URL = env.API_URL;
 
+//*  No Dynamic and No { cache: on-store } : SSG -> Static Page
+//* { cache: on-store } : SSR -> Dynamic Page
+//* next: { revalidate: 10 } : ISR -> Mix between static dynamic
 export const blogService = {
     getBlogPosts: async function () {
 
         try {
 
-            const res = await fetch(`${API_URL}/posts`)
+            const res = await fetch(`${API_URL}/posts`, {next: {revalidate: 10}}); // ISR run  -   {next: {revalidate: 10}}
 
             const data = await res.json();
 
