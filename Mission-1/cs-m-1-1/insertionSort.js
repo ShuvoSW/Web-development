c0013d38e98e406c518bf87a3e8d30b120be4e7e
ShuvoSW <shuvomajumder3369@gmail.com>
2026-01-21 18:08:11 +0600

    Insertion Sort

---
+++ Mission-1/cs-m-1-1/insertionSort.js
@@ -0,0 +1,18 @@
+function iSort(arr) {
+    for(let i = 1; i <arr.length; i++) {
+        let key = arr[i]
+        // console.log(key);
+
+        let j = i -1
+
+        while(j >= 0 && arr[j] >key ) {
+            arr[j + 1] = arr[j]
+
+            j--;
+        }
+        arr [j + 1] = key
+    }
+    return arr;
+}
+const nums = [2, 5, 8, 1, 4]
+console.log(iSort(nums));
