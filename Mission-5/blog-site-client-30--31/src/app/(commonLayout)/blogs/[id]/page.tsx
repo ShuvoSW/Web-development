1adbd521ae29589d01db6128ce82125995e9dde2
ShuvoSW <shuvomajumder3369@gmail.com>
2026-01-21 23:05:59 +0600

    Card view

--- Mission-5/blog-site-client-30--31/src/app/(commonLayout)/blogs/[id]/page.tsx
+++ Mission-5/blog-site-client-30--31/src/app/(commonLayout)/blogs/[id]/page.tsx
@@ -1,20 +1,26 @@
 //! This can be used only in client component
 // import { useParams } from "next/navigation";
 
+import { blogService } from "@/services/blog.service";
+
 export default async function BlogPage({
     params,
 }: {
-    params: Promise<{id: string}>
+    params: Promise<{ id: string }>
 }) {
-    const {id} = await params;
+    const { id } = await params;
 
+    const { data: blog } = await blogService.getBlogById(id)
+
+    // console.log(data);
     // console.log(id);
 
     //! This can be used only in client component
     // const {id} = useParams();
-  return (
-    <div>
-      <h1>This is a dynamic page {id} </h1>
-    </div>
-  );
+    return (
+        <div>
+            <h1> This is a dynamic page  {id} </h1>
+
+        </div>
+    );
 }
