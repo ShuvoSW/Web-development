689b821b40b2b479cf99fc318dcc4fed8f3cc06b
ShuvoSW <shuvomajumder3369@gmail.com>
2026-01-22 20:29:24 +0600

    Problem with using the service layer inside client component

--- Mission-5/blog-site-client-30--31/src/app/(commonLayout)/about/page.tsx
+++ Mission-5/blog-site-client-30--31/src/app/(commonLayout)/about/page.tsx
@@ -1,11 +1,27 @@
+"use client"
 
-export const dynamic = 'force-dynamic';
+import { blogService } from "@/services/blog.service";
+import { useEffect, useState } from "react";
+
+// export const dynamic = 'force-dynamic';
 export default async function AboutPage() {
   //For this simulation load time
-  await new Promise((resolve) => setTimeout(resolve, 4000));
+  // await new Promise((resolve) => setTimeout(resolve, 4000));
 
-    //For this simulation error
-    // throw new Error("Something went wrong");
+  const [data, setData] = useState()
+
+  console.log(data);
+
+  useEffect(() => {
+    (async () => {
+      const { data } = await blogService.getBlogPosts()
+
+      setData(data)
+    })();
+  }, [])
+
+  //For this simulation error
+  // throw new Error("Something went wrong");
   return (
     <div>
       <h1>This is about page component</h1>
